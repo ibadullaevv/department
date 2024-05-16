8bcf3350228c0eb6c80d0e6b52c342c657d9afc3
Xursandbek <ibadullaevkhursandbek@gmail.com>
2024-05-17 00:11:39 +0500

    admin file registered

--- app/admin.py
+++ app/admin.py
@@ -10,15 +10,23 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ('username', 'email', 'is_staff', 'is_active', 'department')
 
-    def save_model(self, request, obj, form, change):
-        if 'password' in form.cleaned_data:
-            password = form.cleaned_data['password']
-            obj.set_password(password)
-            obj.save()
-
-        super().save_model(request, obj, form, change)
+    # def save_model(self, request, obj, form, change):
+    #     if 'password' in form.cleaned_data:
+    #         password = form.cleaned_data['password']
+    #         obj.set_password(password)
+    #         obj.save()
+    #
+    #     super().save_model(request, obj, form, change)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Post)
-admin.site.register(Department)
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'author', 'date')
